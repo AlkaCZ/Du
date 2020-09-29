5e297e7bbd8a6b8366a35e34675b5125baf6755b
Petr Jelínek <jelinek.petr.2017@skola.ssps.cz>
2020-09-29 19:09:38 +0200

    done ?

--- Cars-Úkol-ITVP/Program.cs
+++ Cars-Úkol-ITVP/Program.cs
@@ -12,4 +12,67 @@ namespace Cars_Úkol_ITVP
         {
         }
     }
+     class Auto
+    {
+        public int PocetKol { get; set; }
+        public string Znacka { get; set; }
+        public int PocetSedacek { get; set; }
+        public double Zrychleni { get; set; }
+
+        public double VypocetSpotreby()
+        {
+            double S;
+            S = Zrychleni * 0.1;
+            return S;
+        }
+        public virtual void ToString()
+        {
+            Console.WriteLine("Auto zrychlí z 0 na 100 Km/h za" + " " + Zrychleni);
+        }
+
+        public Auto(int pocetkol, string znacka, int pocetsedacek, double zrychleni)
+        {
+            pocetkol = PocetKol;
+            znacka = Znacka;
+            pocetsedacek = PocetSedacek;
+            zrychleni = Zrychleni;
+        }
+
+    }
+    class Kombi : Auto
+    {
+        public double VekiostKufru { get; set; }
+        public override void ToString()
+        {
+            Console.WriteLine("Velikost kufru je "+VekiostKufru);
+        }
+
+        public Kombi(Auto a, double velikostkufru) : base (a.PocetKol, a.Znacka, a.PocetSedacek, a.Zrychleni)
+        {
+            PocetKol = a.PocetKol;
+            Znacka = a.Znacka;
+            PocetSedacek = a.PocetSedacek;
+            Zrychleni = a.Zrychleni;
+            velikostkufru = VekiostKufru;
+        }
+    }
+
+     class Sedan : Auto
+    {
+        public string Barva { get; set; }
+        public override void ToString()
+        {
+            Console.WriteLine("Vaše auto má " + Barva + " barvu");
+        }
+        public  Sedan(Auto b, string barva) : base(b.PocetKol, b.Znacka, b.PocetSedacek, b.Zrychleni)
+        {
+            PocetKol = b.PocetKol;
+            Znacka = b.Znacka;
+            PocetSedacek = b.PocetSedacek;
+            Zrychleni = b.Zrychleni;
+            barva = Barva;
+        }
+        
+    }
+
 }
