2e6d3a817013f32d1102a206675a5462eeef0f38
Petr Jelínek <jelinek.petr.2017@skola.ssps.cz>
2020-10-04 11:32:44 +0200

    Game added

--- Cars-Úkol-ITVP/Program.cs
+++ Cars-Úkol-ITVP/Program.cs
@@ -6,10 +6,77 @@ using System.Threading.Tasks;
 
 namespace Cars_Úkol_ITVP
 {
+
     class Program
     {
+        Random r = new Random(30);
+        public string ID()
+        {
+            int rand = r.Next();
+            string name = rand.ToString();
+            return name;
+        }
+
         static void Main(string[] args)
         {
+            bool JeHra = true;
+            Auto[] Garage = new Auto[30];
+            int cars = 0;
+            int i = 0;
+
+           
+
+
+                Console.WriteLine("Ahoj, výtej");
+                Console.WriteLine("Podívej se kolik máš aut příkazem : cars");
+                if (Console.ReadLine() == "cars")
+                {
+                    Console.WriteLine(cars);
+                }
+            else
+            {
+                JeHra = false;
+            }
+            Console.WriteLine("Pokud souhlasíte napište > OK");
+            if (Console.ReadLine() == "OK")
+            {
+                Console.Clear();
+                Console.WriteLine("Začni si přidávat nová auta příkazem : addcar");
+                Console.WriteLine("Ale pozor máš místo jen na 30 aut");
+            }
+            else
+            {
+                JeHra = false;
+            }
+
+
+
+            while (JeHra)
+            {
+                if (30 > i)
+                {
+                    if (Console.ReadLine() == "addcar")
+                    {
+                        Auto ID = new Auto();
+                        Garage[i] = ID;
+                        ID.TEST();
+                        i++;
+                        cars++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Máte : " + cars + " aut");
+                        JeHra = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Máte plnou garáž");
+                    JeHra = false;
+                }
+               
+            }   
+            Console.ReadKey();
         }
     }
      class Auto
@@ -28,6 +95,17 @@ namespace Cars_Úkol_ITVP
         public virtual void ToString()
         {
             Console.WriteLine("Auto zrychlí z 0 na 100 Km/h za" + " " + Zrychleni);
+        }
+        public void TEST()
+        {
+            Console.WriteLine("Car Added");
+        }
+        public Auto()
+        {
+            PocetKol = 4;
+            Znacka = "nedefinovana";
+            PocetSedacek = 5;
+            Zrychleni = 20.6;
         }
 
         public Auto(int pocetkol, string znacka, int pocetsedacek, double zrychleni)
